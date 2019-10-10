318ff41b0de005567e2c59a11f102b8ece3844a0
mynamezhaoyu <995680821@qq.com>
2019-10-10 18:13:40 +0800

    feat:完善订单修改

--- controllers/order.js
+++ controllers/order.js
@@ -13,7 +13,7 @@ let obj = {
     let data = await connection.query(str) // 先插入order表，主表
     // orderProjectList 订单产品表
     let str1 = param.projectData.map(r => `('${r.id}', '${r.sort}', '${r.units}', '${r.cost}', '${r.price}', '${r.count}', '${data.insertId}')`).join(',')
-    let data1 = await connection.query(`INSERT INTO orderProjectList (projectID, sort, units, cost, price, count, orderId) values ${str1}`)
+    let data1 = await connection.query(`INSERT INTO orderProjectList (projectId, sort, units, cost, price, count, orderId) values ${str1}`)
     // orderPremium 订单额外费用表
     let str2 = '', data2 = ''
     if (param.premiumData.length) {
@@ -41,8 +41,8 @@ let obj = {
     let data2 = '', data3 = ''
     if (data.length) {
       data2 = await connection.query(`
-        select *, tb.name from orderProjectList ta 
-        left join project tb ON ta.projectID = tb.id
+        select ta.*, tb.name from orderProjectList ta 
+        left join project tb ON ta.projectId = tb.id
         where ta.orderId in (${data.map(r => r.id + '').join()})
       `)
       data3 = await connection.query(`select * from orderPremium ta where ta.orderId in (${data.map(r => r.id + '').join()})`)
@@ -53,5 +53,33 @@ let obj = {
     })
     ctx.body = (Object.assign(global.createObj(), { item: data, str: str, totalCount: data1[0]['count(1)'] }))
   },
+  'POST /editOrder': async (ctx, next) => {
+    let param = ctx.request.body
+    let str = global.edit(
+      [
+        { str: 'phone' }, { str: 'custAddress' },
+        { str: 'sales' }, { str: 'address', data: param.address.join(',') }, { str: 'deliveryType' },
+        { str: 'shipping' }, { str: 'courier' }, { str: 'downPayment' }, { str: 'remark' },
+        { str: 'company' }
+      ], '_order', param)
+    let data = await connection.query(str) // 先更新order表，主表
+    let data1 = await connection.query(`delete from orderProjectList where id not in(${param.projectData.filter(r => r.id).map(r => r.id).join(',') || ''}) and orderId = '${param.id}'`)
+    let data2 = await connection.query(`
+      INSERT INTO orderProjectList (id, projectId, sort, units, cost, price, count, orderId) VALUES 
+      ${param.projectData.map(r => {
+        return `(${r.projectId ? r.id : 0}, '${r.projectId || r.id}', '${r.sort}', '${r.units}', '${r.cost}', '${r.price}', '${r.count}', '${param.id}')`
+      }).join(',')}
+      ON DUPLICATE KEY UPDATE units = VALUES(units), sort = VALUES(sort), cost = VALUES(cost), price = VALUES(price), count = VALUES(count), orderId = VALUES(orderId);
+    `)
+    let data3 = await connection.query(`delete from orderPremium where id not in(${param.premiumData.filter(r => r.id).map(r => r.id).join(',') || ''}) and orderId = '${param.id}'`)
+    let data4 = await connection.query(`
+    INSERT INTO orderPremium (id, name, money, remark, orderId) VALUES 
+    ${param.premiumData.map(r => {
+      return `(${r.id || 0}, '${r.name}', '${r.money}', '${r.remark}', '${param.id}')`
+    }).join(',')}
+    ON DUPLICATE KEY UPDATE name = VALUES(name), money = VALUES(money), remark = VALUES(remark), orderId = VALUES(orderId);
+  `)
+    ctx.body = (Object.assign(global.createObj(), { item: data + ',' + data1 + ',' + data2 + ',' + data3 + ',' + data4 }))
+  }
 }
 module.exports = obj;
