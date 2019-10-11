1ddd95b984778a5a70f0f8af5b4f9d05b2d8fadb
mynamezhaoyu <995680821@qq.com>
2019-10-11 14:15:52 +0800

    feat:订单新增发起日期

--- controllers/order.js
+++ controllers/order.js
@@ -7,7 +7,7 @@ let obj = {
       [
         { str: 'name' }, { str: 'phone' }, { str: 'custAddress' },
         { str: 'sales' }, { str: 'address', data: param.address.join(',') }, { str: 'deliveryType' },
-        { str: 'shipping' }, { str: 'courier' }, { str: 'downPayment' }, { str: 'remark' },
+        { str: 'shipping' }, { str: 'courier' }, { str: 'orderDate' }, { str: 'downPayment' }, { str: 'remark' },
         { str: 'createDate' }, { str: 'createUser' }, { str: 'company' }
       ], '_order', param)
     let data = await connection.query(str) // 先插入order表，主表
@@ -28,12 +28,15 @@ let obj = {
     let special = '1 = 1'
     if (param.id) special = `ta.id = ${param.id}`
     let str = `
-      select ta.*, date_format(ta.createDate, '%Y-%m-%d %H:%I:%S') as createDate1, tb.name as createName , tc.name as custName  , td.name as salesName from _order ta
-      left join user tb ON ta.createUser = tb.id
-      left join customer tc ON ta.name = tc.id
-      left join user td ON ta.sales = td.id
-      where ${special} and ta.company = '${param.company}' and (ta.name like '%${param.value}%' or
-      ta.phone like '%${param.value}%') limit ${arr[0]},${arr[1]}
+      select ta.id, ta.name, ta.phone, ta.custAddress, ta.sales, ta.deliveryType, 
+        ta.address, ta.shipping, ta.courier, ta.downPayment, ta.remark, ta.createUser, ta.company, 
+        date_format(ta.createDate, '%Y-%m-%d %H:%I:%S') as createDate, date_format(ta.orderDate, '%Y-%m-%d') as orderDate,
+        tb.name as createName , tc.name as custName , td.name as salesName from _order ta
+        left join user tb ON ta.createUser = tb.id
+        left join customer tc ON ta.name = tc.id
+        left join user td ON ta.sales = td.id
+        where ${special} and ta.company = '${param.company}' and (ta.name like '%${param.value}%' or
+        ta.phone like '%${param.value}%') limit ${arr[0]},${arr[1]}
     `
     let str1 = `select count(1) from _order ta left join user tb ON ta.createUser = tb.id where ta.company = '${param.company}' and (ta.name like '%${param.value}%' or ta.phone like '%${param.value}%')`
     let data = await connection.query(str)
@@ -59,7 +62,7 @@ let obj = {
       [
         { str: 'phone' }, { str: 'custAddress' },
         { str: 'sales' }, { str: 'address', data: param.address.join(',') }, { str: 'deliveryType' },
-        { str: 'shipping' }, { str: 'courier' }, { str: 'downPayment' }, { str: 'remark' },
+        { str: 'shipping' }, { str: 'courier' }, { str: 'orderDate' }, { str: 'downPayment' }, { str: 'remark' },
         { str: 'company' }
       ], '_order', param)
     let data = await connection.query(str) // 先更新order表，主表
@@ -67,16 +70,16 @@ let obj = {
     let data2 = await connection.query(`
       INSERT INTO orderProjectList (id, projectId, sort, units, cost, price, count, orderId) VALUES 
       ${param.projectData.map(r => {
-        return `(${r.projectId ? r.id : 0}, '${r.projectId || r.id}', '${r.sort}', '${r.units}', '${r.cost}', '${r.price}', '${r.count}', '${param.id}')`
-      }).join(',')}
+      return `(${r.projectId ? r.id : 0}, '${r.projectId || r.id}', '${r.sort}', '${r.units}', '${r.cost}', '${r.price}', '${r.count}', '${param.id}')`
+    }).join(',')}
       ON DUPLICATE KEY UPDATE units = VALUES(units), sort = VALUES(sort), cost = VALUES(cost), price = VALUES(price), count = VALUES(count), orderId = VALUES(orderId);
     `)
     let data3 = await connection.query(`delete from orderPremium where id not in(${param.premiumData.filter(r => r.id).map(r => r.id).join(',') || ''}) and orderId = '${param.id}'`)
     let data4 = await connection.query(`
       INSERT INTO orderPremium (id, name, money, remark, orderId) VALUES 
       ${param.premiumData.map(r => {
-        return `(${r.id || 0}, '${r.name}', '${r.money}', '${r.remark}', '${param.id}')`
-      }).join(',')}
+      return `(${r.id || 0}, '${r.name}', '${r.money}', '${r.remark}', '${param.id}')`
+    }).join(',')}
       ON DUPLICATE KEY UPDATE name = VALUES(name), money = VALUES(money), remark = VALUES(remark), orderId = VALUES(orderId);
   `)
     ctx.body = (Object.assign(global.createObj(), { item: data + ',' + data1 + ',' + data2 + ',' + data3 + ',' + data4 }))
